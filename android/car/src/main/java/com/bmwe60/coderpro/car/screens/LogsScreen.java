3966a02d61d9c56b9a94e7309d18a2a51a18ac74
Kimi <kimi@moonshot.ai>
2026-07-24 11:42:26 +0800

    fix: use existing getLivePerformanceData() in LogsScreen, remove non-existent getLastLiveData() call

--- android/car/src/main/java/com/bmwe60/coderpro/car/screens/LogsScreen.java
+++ android/car/src/main/java/com/bmwe60/coderpro/car/screens/LogsScreen.java
@@ -2,23 +2,18 @@ package com.bmwe60.coderpro.car.screens;
 
 import androidx.annotation.NonNull;
 import androidx.car.app.CarContext;
-import androidx.car.app.CarToast;
 import androidx.car.app.Screen;
 import androidx.car.app.model.Action;
-import androidx.car.app.model.CarColor;
 import androidx.car.app.model.ItemList;
 import androidx.car.app.model.ListTemplate;
 import androidx.car.app.model.Row;
 import androidx.car.app.model.Template;
-import androidx.car.app.model.CarIcon;
-import androidx.core.graphics.drawable.IconCompat;
 
 import com.bmwe60.coderpro.car.R;
 import com.bmwe60.coderpro.car.obd.KDCANManager;
 import java.util.Map;
 
 public class LogsScreen extends Screen {
-    private boolean mIsLogging = false;
 
     public LogsScreen(@NonNull CarContext carContext) {
         super(carContext);
@@ -29,7 +24,7 @@ public class LogsScreen extends Screen {
     public Template onGetTemplate() {
         ItemList.Builder listBuilder = new ItemList.Builder();
         
-        Map<String, Double> liveData = KDCANManager.getInstance().getLastLiveData();
+        Map<String, String> liveData = KDCANManager.getInstance().getLivePerformanceData();
         
         if (liveData.isEmpty()) {
             listBuilder.addItem(new Row.Builder()
@@ -37,10 +32,10 @@ public class LogsScreen extends Screen {
                 .addText("Connect to vehicle to view live data")
                 .build());
         } else {
-            for (Map.Entry<String, Double> entry : liveData.entrySet()) {
+            for (Map.Entry<String, String> entry : liveData.entrySet()) {
                 listBuilder.addItem(new Row.Builder()
                     .setTitle(entry.getKey())
-                    .addText(String.format("%.2f", entry.getValue()))
+                    .addText(entry.getValue())
                     .build());
             }
         }
